56d8b3dfc44b35ad9cd366fb45ebc18915fbe892
Rhmnhsim <115064188+rhmnhsim@users.noreply.github.com>
2025-10-28 06:52:48 +0000

    Update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = new Frog({
 // Frame utama (halaman pertama)
 app.frame('/', (c) => {
   return c.res({
-    image: 'https://zora.co/coin/base:0x431371045e52dac0bd39a722dfc9e76d79913afd?referrer=0xf96c80dab17bccc9e0c0c454fa6ec9234ef240f2', // ganti dengan gambar NFT kamu
+    image: 'https://pbs.twimg.com/profile_images/1965893018315272195/0h5xrsuS_400x400.jpg', // ganti dengan gambar NFT kamu
     intents: [
       {
         label: 'Claim NFT 🎁',
